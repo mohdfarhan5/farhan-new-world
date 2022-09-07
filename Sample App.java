bfdf07db055d278901550a5a14910dec3d967daa
mohdfarhan5 <111415888+mohdfarhan5@users.noreply.github.com>
2022-09-07 15:22:08 +0530

    Add files via upload

---
+++ Sample App.java
@@ -0,0 +1,6 @@
+package sampleapp;
+public class SampleApp {
+       public static void main(String[] args) {
+             System.out.println(("This is a sample project to be upload to github");
+        }
+}
